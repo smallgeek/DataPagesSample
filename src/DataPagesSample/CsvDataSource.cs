c9643963d438de297a615e73b036b60354ec2bbe
smallgeek <smallgeek@outlook.jp>
2016-11-12 14:55:18 +0900

    Create sample skeleton

---
+++ src/DataPagesSample/CsvDataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Pages;
+
+namespace DataPagesSample
+{
+    public class CsvDataSource : BaseDataSource
+    {
+        public string Path { get; set; }
+
+        protected override Task<IList<IDataItem>> GetRawData()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object GetValue(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool SetValue(string key, object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
